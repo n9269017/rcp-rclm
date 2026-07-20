c3491b0e5e14395329086cfd2e6ce9954dbd1c6e
n9269017 <n926fl@gmail.com>
2026-07-20 03:13:48 -0400

    Export Phase 11A generator surface

---
+++ python/rcp_rclm_runtime_v3/rcp_rclm_runtime_v3/phase11/__init__.py
@@ -0,0 +1,61 @@
+from rcp_rclm_runtime_v3.phase11.bootstrap import (
+    Phase11BootstrapFixture,
+    build_phase11_bootstrap,
+    validate_phase11_bootstrap_package,
+)
+from rcp_rclm_runtime_v3.phase11.generator import (
+    generate_typed_mutation_program,
+    phase11_objective_hash,
+    validate_generated_program,
+)
+from rcp_rclm_runtime_v3.phase11.grammar import (
+    encode_typed_mutation_program,
+    parse_typed_mutation_program,
+)
+from rcp_rclm_runtime_v3.phase11.records import (
+    ArchitectureMutationDirective,
+    BudgetLedger,
+    DataSelectionDirective,
+    GeneratorDecodeStep,
+    GeneratorInvocationReport,
+    InvocationBudget,
+    ModelGeneratorInput,
+    Phase11ReasonCode,
+    ProgramValidationReport,
+    ResourceRequest,
+    RollbackDeclaration,
+    TrainingDirective,
+    TypedMutationProgram,
+    default_phase11_budget,
+)
+from rcp_rclm_runtime_v3.phase11.reference import (
+    Phase11AReference,
+    build_phase11a_reference,
+)
+
+__all__ = [
+    "ArchitectureMutationDirective",
+    "BudgetLedger",
+    "DataSelectionDirective",
+    "GeneratorDecodeStep",
+    "GeneratorInvocationReport",
+    "InvocationBudget",
+    "ModelGeneratorInput",
+    "Phase11AReference",
+    "Phase11BootstrapFixture",
+    "Phase11ReasonCode",
+    "ProgramValidationReport",
+    "ResourceRequest",
+    "RollbackDeclaration",
+    "TrainingDirective",
+    "TypedMutationProgram",
+    "build_phase11_bootstrap",
+    "build_phase11a_reference",
+    "default_phase11_budget",
+    "encode_typed_mutation_program",
+    "generate_typed_mutation_program",
+    "parse_typed_mutation_program",
+    "phase11_objective_hash",
+    "validate_generated_program",
+    "validate_phase11_bootstrap_package",
+]
